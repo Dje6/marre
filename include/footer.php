0bc4ad18d5db6ef2aa4daac84cffa5df1b3f52fe
DWOcelot <benoit.blondel12@gmail.com>
2016-11-23 16:35:33 +0100

    creation header footer style

---
+++ include/footer.php
@@ -0,0 +1,7 @@
+
+
+    <footer>
+      <h1 class="footerPage">Copyright Rang Fillon ©</h1>
+    </footer>
+  </body>
+</html>
---
+++ include/header.php
@@ -0,0 +1,14 @@
+<?php include 'include/functions.php'; ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Projet Kilian, Hermelen, Jessy, Benoît</title>
+  </head>
+  <body>
+    <header>
+      <h1 class="titreProjet">Projet des zigotos Kilian, Hermelen, Jessy, Benoît</h1>
+    </header>
