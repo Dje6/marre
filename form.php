f8085b98a83d614e1ffbf6e489f47f9f1c4412d4
hermelen <hermelen.peris@gmail.com>
2016-11-23 16:56:45 +0100

    Commit Hermelen 03

---
+++ form.php
@@ -0,0 +1,26 @@
+<div class="">
+  <form class="container" action="index.html" method="post">
+    <div class="form-group">
+    <label for="nom"></label>
+    <input class="form-control" type="text" name="nom" value="" placeholder="Votre nom">
+    </div>
+    <label for="prenom"></label>
+    <input class="form-control" type="text" name="prenom" value="" placeholder="Votre prénom">
+
+    <label for="mail"></label>
+    <input class="form-control" type="text" name="mail" value="" placeholder="Votre adresse e-mail">
+
+    <label for="age"></label>
+    <input class="form-control" type="text" name="age" value="" placeholder="Votre âge">
+
+    <label for="sexe"></label>
+    <input class="form-control" type="text" name="sexe" value="" placeholder="h ou f">
+
+    <label for="taille_du_sexe"></label>
+    <input class="form-control" type="text" name="taille_du_sexe" value="" placeholder="Taille de votre sexe en cm">
+
+    <input type="submit" name="submit" value="Envoyer">
+  </form>
+  <img src="assets/img/anaconda.gif" alt="Nicky danse">
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/LDZX4ooRsWs" frameborder="0" allowfullscreen></iframe>
+</div>
